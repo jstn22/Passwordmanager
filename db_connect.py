433571772e478c67cbaee3c86eb8a2e0f3e8cbc1
justinschockel <jschoeckel22@gmail.com>
2025-03-03 18:44:38 +0100

    add files

--- db_connect.py
+++ db_connect.py
@@ -1,12 +1,9 @@
 import mysql.connector
 
-def db_conection():
-    db = mysql.connector.connect(
+def db_connection():
+    return mysql.connector.connect(
         host="localhost",
         user="root",
         password="",
         database="user"
     )
-
-
-
